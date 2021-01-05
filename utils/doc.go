353220a6fac362eba64a50b8ef62c76cb17ac055
CZ <cz.theng@gmail.com>
2021-01-05 20:12:23 +0800

    [TASK] add utils

---
+++ utils/doc.go
@@ -0,0 +1,10 @@
+// Copyright CZ. All rights reserved.
+// Author: CZ cz.theng@gmail.com
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE fil
+
+/**
+* package utils provides a bunch of utility functions for czkit
+**/
+
+package utils
